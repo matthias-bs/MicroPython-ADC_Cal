240341e7727c0c14b5a4e88d842b2f7f0a12caf7
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-02-04 19:11:50 +0100

    Update ci.sh

--- tools/ci.sh
+++ tools/ci.sh
@@ -12,7 +12,7 @@ function ci_code_formatting_setup {
 }
 
 function ci_code_formatting_run {
-    python3 tools/codeformat.py -v
+    python3 tools/codeformat.py -v adc1_cal.py
 }
 
 ########################################################################################
