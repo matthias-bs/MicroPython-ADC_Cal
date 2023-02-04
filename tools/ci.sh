c493dc0c1d7a7ba1701aab4b498436dd80b39537
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-02-04 21:46:20 +0100

    Update ci.sh

--- tools/ci.sh
+++ tools/ci.sh
@@ -27,6 +27,8 @@ function ci_build_packages_setup {
     # check the required programs run
     /tmp/micropython/mpy-cross/build/mpy-cross --version
     python3 /tmp/micropython/tools/manifestfile.py --help
+    ls tmp/micropython/mpy-cross/
+    ls tmp/micropython/mpy-cross/build
     tmp/micropython/mpy-cross/build/mpy-cross adc1_cal.py
 }
 
