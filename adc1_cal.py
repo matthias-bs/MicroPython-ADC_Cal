fa54fc7fbdf6eb7a0788fb45bcb173ce99b2d0f6
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-02-04 09:35:11 +0100

    Update adc1_cal.py

--- adc1_cal.py
+++ adc1_cal.py
@@ -112,7 +112,6 @@ _VREF_OFFSET            = const(1100)
 _VREF_STEP_SIZE         = const(7)
 _VREF_FORMAT            = const(0)
 _VREF_MASK              = const(0x1F)
-
 _LUT_VREF_LOW           = const(1000)
 _LUT_VREF_HIGH          = const(1200)
 _LUT_ADC_STEP_SIZE      = const(64)
@@ -123,51 +122,52 @@ _LUT_HIGH_THRESH        = (_LUT_LOW_THRESH + _LUT_ADC_STEP_SIZE)
 # 20 Point lookup tables, covering ADC readings from 2880 to 4096, step size of 64
 # LUT for VREF 1000mV
 _lut_adc1_low = [
-  2240,
-  2297,
-  2352,
-  2405,
-  2457,
-  2512,
-  2564,
-  2616,
-  2664,
-  2709,
-  2754,
-  2795,
-  2832,
-  2868,
-  2903,
-  2937,
-  2969,
-  3000,
-  3030,
-  3060
+    2240,
+    2297,
+    2352,
+    2405,
+    2457,
+    2512,
+    2564,
+    2616,
+    2664,
+    2709,
+    2754,
+    2795,
+    2832,
+    2868,
+    2903,
+    2937,
+    2969,
+    3000,
+    3030,
+    3060
 ]
 
 # LUT for VREF 1200mV
 _lut_adc1_high = [
-  2667,
-  2706,
-  2745,
-  2780,
-  2813,
-  2844,
-  2873,
-  2901,
-  2928,
-  2956,
-  2982,
-  3006,
-  3032,
-  3059,
-  3084,
-  3110,
-  3135,
-  3160,
-  3184,
-  3209
+    2667,
+    2706,
+    2745,
+    2780,
+    2813,
+    2844,
+    2873,
+    2901,
+    2928,
+    2956,
+    2982,
+    3006,
+    3032,
+    3059,
+    3084,
+    3110,
+    3135,
+    3160,
+    3184,
+    3209
 ]
+
 
 #################################################################################
 # ADC1Cal class - ADC voltage output using V_ref calibration value and averaging
